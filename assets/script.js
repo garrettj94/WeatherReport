b100904bca9e267692af1da8f333034c3d47bac4
Garrett Jackson <garrettjackson43@gmail.com>
2022-08-12 16:59:26 -0400

    made changes to java css and the index

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,75 @@
 var fetchButton = document.querySelector(".fetchbtn")
+const api = 'f18d365e081872f946b3849bff8f041f';
 
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        for (var i = 0; i < data.length; i++) {
-          // Creating elements, tablerow, tabledata, and anchor
-          var createTableRow = document.createElement('tr');
-          var tableData = document.createElement('td');
-          var link = document.createElement('a');
-  
-          // Setting the text of link and the href of the link
-          link.textContent = data[i].html_url;
-          link.href = data[i].html_url;
-  
-          // Appending the link to the tabledata and then appending the tabledata to the tablerow
-          // The tablerow then gets appended to the tablebody
-          tableData.appendChild(link);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-        }
-      });
-  }
-  
-  fetchButton.addEventListener('click', getApi);
-  
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // Accesing Geolocation of User
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            // Storing Longitude and Latitude in variables
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const base = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+
+            fetch(base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                    //Loop over the data to generate a table, each table row will have a link to the repo url
+                    for (var i = 0; i < data.length; i++) {
+                        // Creating elements, tablerow, tabledata, and anchor
+                        var createTableRow = document.createElement('tr');
+                        var tableData = document.createElement('td');
+                        var link = document.createElement('a');
+
+                        // Setting the text of link and the href of the link
+                        link.textContent = data[i].html_url;
+                        link.href = data[i].html_url;
+
+                        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                        // The tablerow then gets appended to the tablebody
+                        tableData.appendChild(link);
+                        createTableRow.appendChild(tableData);
+                        tableBody.appendChild(createTableRow);
+           }   });
+        });
+    }
+});
+
+
+
+
+
+
+
+
+fetchButton.addEventListener('click', getApi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
